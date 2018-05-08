c9fe74e400eabacfea3821cd3562c41bbf5bf046
ChrisStavnes <ChrisStavnes@gmail.com>
2018-05-07 20:12:07 -0500

    final homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 const mysql= require('mysql');
+const cTable = require('console.table');
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -8,44 +10,73 @@ const connection = mysql.createConnection({
   database: "bamazon"
   });
 
-  connection.connect() ;
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    
+  });
+  
 
-  // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  //   if (error) throw error;
-  //   console.log('The solution is: ', results[0].solution);
-  // });
-   
-  // connection.end();
 
 function displayAllProducts() {
   connection.query(`SELECT * FROM products`, function (error, results, fields) {
 
     if (error) throw error;
-    console.log(results)
+    //console.log(results)
+    const table = cTable.getTable(results);
+    
+    console.log(table)
+    
   });
 };
 
 displayAllProducts();
 
+function purchase() {
+  inquirer
+    .prompt([
+      {
+      name: "buy",
+      type: "checkbox",
+      message: "What woud you like to purchase?",
+      choices: ["sunglasses", "nike shoes", "tent", "iPhone", "watch", "xbox", "latern", "bike", "golf clubs", "inline skates"]
+     },
+     {
+     name: "units",
+     type: "checkbox",
+     message: "How many units would you like to purchase?",
+     choices: ["1", "5", "100"]
+     }
+    ]
+    )
+    .then(function (response) {
+      //console.log(response.buy + response.units)
+
+      let product = response.buy
+      let quantity = response.units
+
+      //let queryStr = `SELECT * FROM products WHERE;`
+
+      connection.query(`SELECT * FROM products WHERE product_name ='${product}'`, function(err, data) {
+        if (err) throw err;
+        // console.log(data)
+        // console.log(data[0].stock_quantity)
+        
+        if (quantity >= data[0].stock_quantity) {
+          console.log("There is not enough inventory to complete your order!")
+          purchase()
+        } else {
+          console.log("Order Placed!\n")
+
+          console.log('Order Total: $'+data[0].price * quantity+"\n")
+          purchase()
+        }
+      })
+
+    })
+  }
+
+purchase();
 
 
-  
-// function getSongByArtist (artist){
-//     connection.query(`SELECT * FROM songs WHERE artist ='${artist}'`, function (error, results, fields) {
-   
-//         if (error) throw error;
-//         console.log(results)
-//       });
-// }  
 
-// function getRepeats (){
-//     connection.query('SELECT artist , COUNT(*) FROM songs GROUP BY artist HAVING COUNT(*) > 2', function (error, results, fields) {
-//         if (error) throw error;
-//         console.log(results)
-//       });
-// }  
-
-
-// // getSongByArtist('Baltimora');
-
-// getRepeats();
